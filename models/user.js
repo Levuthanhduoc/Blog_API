82d4d830d772b9fe38913cb90082c7650308a152
levuthanhduoc <levuthanhduoc@gmail.com>
2024-04-27 15:48:04 +0700

    update user model

--- models/user.js
+++ models/user.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 const Schema =mongoose.Schema;
 
 const UserSchema = new Schema({
-    name:{type:String,required:true,maxLength:1000},
+    role:{type:String,required:true,enum:["user","admin"]},default:"user",
     username:{type:String,required:true,index:{unique:true}},
     password:{required:true,type:String},
+    salt:{required:true,type:String},
 })
 
 module.exports = mongoose.model("User",UserSchema);
