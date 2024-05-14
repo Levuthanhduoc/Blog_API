a96bb738ff265cfd7a2a82fc8716dfb20b22a295
levuthanhduoc <levuthanhduoc@gmail.com>
2024-05-14 16:39:04 +0700

    fix incorrect field

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema =mongoose.Schema;
 
 const UserSchema = new Schema({
-    role:{type:String,required:true,enum:["user","admin"]},default:"user",
+    role:{type:String,required:true,enum:["user","admin"],default:"user"},
     username:{type:String,required:true,index:{unique:true}},
     password:{required:true,type:String},
     salt:{required:true,type:String},
