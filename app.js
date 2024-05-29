40c0290838cabcf34602df2047c90de2a64c410a
levuthanhduoc <levuthanhduoc@gmail.com>
2024-05-29 10:25:02 +0700

    clean code

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config();
+const user = require("./controller/userController");
 const mongoose = require('mongoose');
 
 mongoose.set("strictQuery",false);
@@ -15,6 +16,7 @@ async function main() {
 
 
 var indexRouter = require('./routes/index');
+const userRouter = require('./routes/users');
 
 var app = express();
 
@@ -27,8 +29,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use
 
 app.use('/', indexRouter);
+app.use("/users",user.verifyUser,userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
