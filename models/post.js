2d35ad6e2b31dbe769ae869046a0a049028abdbd
levuthanhduoc <levuthanhduoc@gmail.com>
2024-05-29 10:19:04 +0700

    Update post model

--- models/post.js
+++ models/post.js
@@ -3,13 +3,19 @@ const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
     title:{type:String,required:true},
-    content:[{type:String,required:true}],
+    content:{type:String,required:true},
     time:{type:Date},
     status:{type:String,
         required:true,
-        enum:[published,unpublished],
+        enum:["published","unpublished"],
         default:"unpublished",
-    }
+    },
+    author:{type:Schema.Types.ObjectId,ref:"User"},
+    isEdited:{type:Boolean,default:false},
+})
+
+PostSchema.virtual("url").get(function(){
+    return `/users/post/history/${this._id}`;
 })
 
 module.exports = mongoose.model("Post", PostSchema);
